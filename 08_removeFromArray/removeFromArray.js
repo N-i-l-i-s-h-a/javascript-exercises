5867bee8fa083b14571e52ac08a9b18179478db0
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-07-28 11:55:59 +0000

    modify removeFromArray again by using includes() directly on the rest parameter

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,6 +1,5 @@
 const removeFromArray = function(arr, ...num) {
-    const args = Array.from(num);
-    return arr.filter((item) => !args.includes(item)); 
+    return arr.filter((item) => !num.includes(item)); 
 };
 
 // Do not edit below this line
