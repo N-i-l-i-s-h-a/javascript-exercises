dc2c3da1f165453fbed02044cc47572b452998c0
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-07-28 11:53:25 +0000

    modify 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,8 +1,6 @@
 const removeFromArray = function(arr, ...num) {
     const args = Array.from(num);
-    return arr.filter(function remove(item){
-        return !(args.includes(item));
-    }); 
+    return arr.filter((item) => !args.includes(item)); 
 };
 
 // Do not edit below this line
