8cbcbcdfa541dbd7e0a87e75c2909251c2039419
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-07-28 14:56:34 +0000

    solve sumAll i.e. exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0)
+        return "ERROR";
+    return (Math.abs(b - a) + 1) * (a + b) / 2;
 };
 
 // Do not edit below this line
