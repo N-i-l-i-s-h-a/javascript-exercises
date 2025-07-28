6ab22559528cedbb605e9030c7de28bc9e72766b
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-07-28 15:10:12 +0000

    complete exercise 11: tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let fah = (temp - 32) * 5 / 9;
+  return parseFloat(fah.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let cel = ((temp * 9) / 5) + 32;
+  return parseFloat(cel.toFixed(1)); 
 };
 
 // Do not edit below this line
